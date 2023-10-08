e4b54f2bdc08337a9d2d40cdbd749c8f9be2e3f7
Oyetola israel <oyetolaisreal01@gmail.com>
2023-10-08 11:44:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
 	write(2,
-	"and that piece of art is useful\" - Dora korper,2015-10-19\n",59);
+	"and that piece of art is useful\" - Dora korper,2015-10-19\n", 59);
 	return (1);
-}	
+}
