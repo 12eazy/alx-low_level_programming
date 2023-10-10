d977ae1bf051b8949fe5edd6264be24e6f096ee1
Oyetola israel <oyetolaisreal01@gmail.com>
2023-10-09 17:31:12 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	int n, m, l;
 
-	for (n = 0; n < 8; n++)
+	for (n =  48; n < 58; n++)
 	{
-		for (m = 1; m < 9; m++)
+		for (m = 49; m < 58; m++)
 		{
-			for (l = 2; l < 9; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
 					putchar(l);
-					if (n != 8 || m != 9)
+					if (n != 55 || m != 50)
 					{
 						putchar(',');
 						putchar(' ');
