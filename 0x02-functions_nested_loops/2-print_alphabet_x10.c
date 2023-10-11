f0719f53623e90e868da9d7ad65c1da817ade633
Oyetola israel <oyetolaisreal01@gmail.com>
2023-10-11 00:16:00 -0700

    2-alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - print alphabet ten times
+ * print_alphabet_x10 - print alphabet in lower case
  * Return: always 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int i;
 	char j;
@@ -13,11 +13,7 @@ void print_alphabet_x10(void);
 	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 'a' ; j <= 'z' ; j++)
-			putchar(j);
+			_putchar(j);
+		_putchar('\n');
 	}
-	putchar('\n');
-
-	return (0);
-
-
 }
