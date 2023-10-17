435211c923cb651da046653b71f9b686b6c62f3f
Oyetola israel <oyetolaisreal01@gmail.com>
2023-10-17 03:54:33 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverse a string
+ * @s: string to be reversed
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int k;
+	int b;
+	int count = 0;
+	
+	for (k = 0 ; s[k] != '\0' ; k++)
+		count++;
+	for (k = 0 ; k < count / 2 ; k++)
+	{
+		b = s[k];
+		s[k] = s[count - 1 - k];
+		s[count - 1 - k] = b;
+
+	}
+}
