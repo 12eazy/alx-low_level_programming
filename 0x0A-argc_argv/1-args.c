42ce60b31493d9429899e222a5b0cbd60c21804c
Oyetola israel <oyetolaisreal01@gmail.com>
2023-10-30 17:14:48 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 /**
  * main - print the number of argument
  * @argc: argument count
- * #argv: argument vector
+ * @argv: argument vector
  * Return: 0
  */
 
@@ -11,6 +11,6 @@ int main(int argc, char *argv[])
 {
 	(void) argv;
 
-	printf("%d\n" , argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
