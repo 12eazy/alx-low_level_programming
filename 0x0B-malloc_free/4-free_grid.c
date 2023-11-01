704fa9aba54d97283f306a0332e08b1f309aef06
Oyetola israel <oyetolaisreal01@gmail.com>
2023-11-01 16:41:45 -0700

    4treet

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* free_grid - a function that frees a 2 dimensional grid
+* @grid: the 2 dimensional grid
+* @height: height of the grid
+* Return: nothing
+*/
+void free_grid(int **grid, int height)
+{
+	int k;
+
+	for (k = 0; k < height; k++)
+	{
+		free(grid[k]);
+	}
+	free(grid);
+}
