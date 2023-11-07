fb81cf5679771a16d809a1770899c78f80a30dad
Oyetola israel <oyetolaisreal01@gmail.com>
2023-11-07 15:12:26 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ * @name: name of dpg
+ * @age: age of dog
+ * @owner: owner of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
