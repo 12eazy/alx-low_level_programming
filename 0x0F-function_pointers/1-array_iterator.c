a478ca9c59f1260fcea7dc3601771526769808c7
Oyetola israel <oyetolaisreal01@gmail.com>
2023-11-08 21:14:27 -0800

    function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * array_iteration - execute a function on each element of nan array
+ * array_iterator - execute a function on each element of nan array
  * @array: the array
- * @size: size of the arrzy
+ * @size: size of the array
  * @action: function to perform on each element of array
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int k;
-
+i
 	if (array == NULL || action == NULL)
 		return;
 	for (k = 0; k < size; k++)
