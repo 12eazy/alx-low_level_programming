816094c6b9c62c904a8f3a6996b693bab136be34
Oyetola israel <oyetolaisreal01@gmail.com>
2023-11-09 15:18:50 -0800

    variable

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print number
+ * @separator: string to be printed between numbers
+ * @n: number of argument
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int k;
+	va_list bee;
+
+	va_start(bee, n);
+	for (k = 1; k <= n; k++)
+	{
+		if (separator != NULL)
+		{
+			printf("%d", va_arg(bee, int));
+			printf("%s", separator);
+		}
+		else
+			printf("%d", va_arg(bee, int));
+	}
+	printf("\n");
+	va_end(bee);
+}
