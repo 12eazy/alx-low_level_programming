08f739c3f68c164109d5d310658222dfbb27a6ec
Oyetola israel <oyetolaisreal01@gmail.com>
2024-01-19 18:09:08 -0800

    create a dynamic

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif /*_MAIN_H_*/
